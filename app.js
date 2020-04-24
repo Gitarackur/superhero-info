546e03258d1e78fcedcaf574f74d265591872cf6
Gitarackur---Peter Osah <Peterosah5@gmail.com>
2020-04-24 04:55:01 +0100

    On branch master

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-// fetch("https://superhero-search.p.rapidapi.com/?hero=spiderman", {
-//     "method": "GET",
-//     "headers": {
-//       "x-rapidapi-host": "superhero-search.p.rapidapi.com",
-//       "x-rapidapi-key": "280c756bf2mshe920eb0eb1ce901p16668bjsn010ad9d415f4"
-//     }
-//   })
-//   .then(data => data.json())
-//   .then(data => {
-//     console.log(data)
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   });
+fetch("https://superhero-search.p.rapidapi.com/?hero=spiderman", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "superhero-search.p.rapidapi.com",
+      "x-rapidapi-key": "280c756bf2mshe920eb0eb1ce901p16668bjsn010ad9d415f4"
+    }
+  })
+  .then(data => data.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(err => {
+    console.error(err);
+  });
